be6da0850cc186dddd7c0fdde3c3090b88305845
Arushi2006 <72396009+Arushi2006@users.noreply.github.com>
2020-10-10 00:41:18 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var car,wall;
+var speed,weight;
+
+function setup()
+{  
+    createCanvas(1600,400);
+    speed=random(55,90);
+    weight=random(400,1500);
+    car=createSprite(50,200,50,50);
+    car.velocityX=speed;
+    wall=createSprite(1500,200,60,800);
+    wall.shapeColor=blue(80,80,80);
+}
+
+function draw()
+{
+    background(0);
+    drawSprites;
+
+    if(wall.x-car.x(car.width+wall.width)/2)
+    }
+        car.velocityX=0;
+        var d=0.5*speed*speed/22509*weight;
+        if(d>180)
+        {
+            car.shapeColor=red(225,0,0);
+        }
+        if(d<180 && d>100)
+          {
+            car.shapeColor=yellow(230,230,0);
+          }
+        if(d<100)
+          {
+            car.shapeColor=green(0,225,0);
+          }
+    }
+}
